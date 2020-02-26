028048d2968c80c05f37927fd2d3fe74da31e611
mjansen <mjansen@databay.de>
2020-02-26 18:06:15 +0100

    Set exam_start regular expression to exam_take
    This is done according to the API, because the exam_start expression
    MUST match the launch URL.

--- classes/Webservice/Rest/Impl.php
+++ classes/Webservice/Rest/Impl.php
@@ -140,6 +140,9 @@ class Impl implements Rest
             $refId, $refId
         );
 
+        // 2020-02-26: Set $startRegex = $takeRegex, because it MUST match the Launch URL according to the API 
+        $startRegex = $takeRegex;
+
         $this->logger->debug(sprintf(
             "Regular Expressions: Start Exam: %s / Take Exam: %s / End Exam: %s",
             $regexQuotedBaseUrlWithScript . $startRegex,
