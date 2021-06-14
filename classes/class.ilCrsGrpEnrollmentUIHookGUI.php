67043551bd7fe6e208081b037bb9268998e66524
mjansen <mjansen@databay.de>
2021-06-14 14:58:58 +0200

    Add ILIAS 6 and 7 support

--- classes/class.ilCrsGrpEnrollmentUIHookGUI.php
+++ classes/class.ilCrsGrpEnrollmentUIHookGUI.php
@@ -55,15 +55,10 @@ class ilCrsGrpEnrollmentUIHookGUI extends ilUIHookPluginGUI
     {
         if (
             !isset($this->dic['tpl']) ||
+            !isset($this->dic['refinery']) ||
             !isset($this->dic['ilToolbar'])
         ) {
             return;
-        }
-
-        if (version_compare(ILIAS_VERSION_NUMERIC, '6.0', '>=')) {
-            if (!isset($this->dic['refinery'])) {
-                return;
-            }
         }
 
         if (null !== self::$modifiers) {
