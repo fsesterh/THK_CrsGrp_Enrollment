7d3a147a3d054afc95797c155d77c34a2948c982
Fabian Sesterhenn <fabian.sesterhenn@th-koeln.de>
2021-08-27 11:11:19 +0200

    changed resposible to FS

--- plugin.php
+++ plugin.php
@@ -8,5 +8,5 @@ $id = 'crs_grp_enrol';
 $version = '1.0.5';
 $ilias_min_version = '5.4.0';
 $ilias_max_version = '5.4.999';
-$responsible = 'Timo Müller';
-$responsible_mail = 'timomueller@databay.de';
+$responsible = 'Fabian Sesterhenn';
+$responsible_mail = 'fabian.sesterhenn@th-koeln.de';
