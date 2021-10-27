155521da6c3feedf45a3e76809f16526c33c0e9a
Lukas Scharmer <lscharmer@databay.de>
2021-10-27 12:47:39 +0200

    Add language variables. Fix escaped links

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * @author Timo Müller <timomueller@databay.de>
  */
 $id = 'crs_grp_enrol';
-$version = '1.0.6';
+$version = '1.0.7';
 $ilias_min_version = '5.4.0';
 $ilias_max_version = '5.4.999';
 $responsible = 'Timo Müller';
