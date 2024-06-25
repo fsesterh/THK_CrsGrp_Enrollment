14b0b39603562686930b740517b1bbe66af18df7
Marvin Beym <mBeym@databay.de>
2024-06-25 13:21:02 +0200

    Increment plugin version

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@
  * @author Timo Müller <timomueller@databay.de>
  */
 $id = 'crs_grp_enrol';
-$version = '8.0.0';
+$version = '8.1.0';
 $ilias_min_version = '8.0';
 $ilias_max_version = '8.999';
 $responsible = 'Fabian Sesterhenn';
